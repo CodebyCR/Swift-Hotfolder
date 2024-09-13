db6865759fbb5d7fceae58e242c4ad55b950dc5d
Christoph <christoph_rohde@icloud.com>
2024-09-13 17:22:52 +0200

    Add enumerationOptions with default initalisation to initilizer
    - update typo WatcherConfigErrors

--- Sources/Swift-Hotfolder/structs/WatcherConfig.swift
+++ Sources/Swift-Hotfolder/structs/WatcherConfig.swift
@@ -11,11 +11,13 @@ public struct WatcherConfig: Codable {
     let createNonExistingFolders: Bool
     let watchInterval: Double
     let maxHotfolderCount: UInt16
+    let enumerationOptions: FileManager.DirectoryEnumerationOptions
 
     enum CodingKeys: String, CodingKey {
         case createNonExistingFolders = "create_non_existing_folders"
         case watchInterval = "watch_interval"
         case maxHotfolderCount = "max_hotfolder_count"
+        case enumerationOptions = "enumeration_options"
     }
 
     /// For the **safe** default one.
@@ -23,24 +25,38 @@ public struct WatcherConfig: Codable {
         self.createNonExistingFolders = true
         self.watchInterval = 1.0
         self.maxHotfolderCount = 5
+        self.enumerationOptions = []
     }
 
     public static let `default` = WatcherConfig()
 
     /// For maybe **unsafe** WatcherConfig created be the user of the framework.
-    public init?(createNonExistingFolders: Bool, watchInterval: Double, maxHotfolderCount: UInt16) throws {
+    public init?(createNonExistingFolders: Bool, watchInterval: Double, maxHotfolderCount: UInt16, enumerationOptions: FileManager.DirectoryEnumerationOptions = []) throws {
         let minWatcherInterval = 0.1
         guard watchInterval >= minWatcherInterval else {
-            throw WatcherConfigError.watcherIntervalToShort("Your watcherInterval is to short ( < 0.01). Set it up to avoid unexpeted overlabs.")
+            throw WatcherConfigError.watcherIntervalTooShort("Your watcherInterval is to short ( < 0.01). Set it up to avoid unexpeted overlabs.")
         }
 
         guard maxHotfolderCount >= 1 else {
-            throw WatcherConfigError.maxHotfolderCountToSmall("Your maxHotfolderCount is smaller than '1' which make no sense.")
+            throw WatcherConfigError.maxHotfolderCountTooSmall("Your maxHotfolderCount is smaller than '1' which make no sense.")
         }
 
         self.createNonExistingFolders = createNonExistingFolders
         self.watchInterval = watchInterval
         self.maxHotfolderCount = maxHotfolderCount
+        self.enumerationOptions = enumerationOptions
+    }
+
+    /// Custom initializer to decode from any Decoder
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.createNonExistingFolders = try container.decode(Bool.self, forKey: .createNonExistingFolders)
+        self.watchInterval = try container.decode(Double.self, forKey: .watchInterval)
+        self.maxHotfolderCount = try container.decode(UInt16.self, forKey: .maxHotfolderCount)
+
+        // Bitmask as UInt decoding
+        let rawEnumerationOptions = try container.decode(UInt.self, forKey: .enumerationOptions)
+        self.enumerationOptions = FileManager.DirectoryEnumerationOptions(rawValue: rawEnumerationOptions)
     }
 
     /// Try to read the given path to a config json file or return the 'default' WatcherConfig.
