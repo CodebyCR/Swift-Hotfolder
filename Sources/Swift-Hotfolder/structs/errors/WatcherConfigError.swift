a1381345a7282b7e0fb5dc2ae5e1f15bc429ff41
Christoph <christoph_rohde@icloud.com>
2024-09-13 17:09:06 +0200

    Enhance WatcherConfigErrors
    - remove typos
    - replace 'Error' conformance with 'LocalizedError' conformance
    - add errorDescription property

--- Sources/Swift-Hotfolder/structs/errors/WatcherConfigError.swift
+++ Sources/Swift-Hotfolder/structs/errors/WatcherConfigError.swift
@@ -7,9 +7,19 @@
 
 import Foundation
 
-public enum WatcherConfigError: Error {
-    case watcherIntervalToShort(String)
-    case maxHotfolderCountToSmall(String)
+public enum WatcherConfigError: LocalizedError {
+    case watcherIntervalTooShort(String)
+    case maxHotfolderCountTooSmall(String)
     case jsonFileNotFound(String)
     case jsonParsingError(String)
+
+    public var errorDescription: String? {
+        switch self {
+        case .watcherIntervalTooShort(let message),
+             .maxHotfolderCountTooSmall(let message),
+             .jsonFileNotFound(let message),
+             .jsonParsingError(let message):
+            return message
+        }
+    }
 }
