faaa103192d720a926ad577f0ec8f051c8a2ffc1
Christoph <christoph_rohde@icloud.com>
2024-09-15 17:24:10 +0200

    Modify HotfolderWatcher
    Breaking changes:
    
    - remove expect an Hotfolder struct now instead of an URL
    - add don't consuming the gven Hotfolder anymore
    - imporve logging

--- Sources/Swift-Hotfolder/actors/HotfolderWatcher.swift
+++ Sources/Swift-Hotfolder/actors/HotfolderWatcher.swift
@@ -7,7 +7,7 @@
 
 import Combine
 import Foundation
-import os
+import os.log
 
 @globalActor
 public final actor HotfolderWatcher: GlobalActor {
@@ -16,7 +16,9 @@ public final actor HotfolderWatcher: GlobalActor {
     private let uptime: Date
 
     private(set) var hotfolders: Set<Hotfolder> = []
-    private(set) var task: Task<Void, Never>? = nil
+    private(set) var knownFiles: [Hotfolder: [String: Date]] = [:]
+    private var runLoopTask: Task<Void, Never>?
+
     private(set) var isRunning: Bool
 
     public var count: Int {
@@ -35,12 +37,74 @@ public final actor HotfolderWatcher: GlobalActor {
         category: String(describing: HotfolderWatcher.self)
     )
 
-    public final func stop() {
-        // TODO: implement
+    @discardableResult
+    public final func stop() -> Bool {
+        guard isRunning else {
+            Self.logger.warning("""
+            \(#file) -> \(#function) \(#line):\(#column)
+            No task is running.
+            """)
+            return false
+        }
+
+        runLoopTask?.cancel()
+        isRunning = false
+        Self.logger.info("Background task stopped.")
+        return true
     }
 
     @discardableResult
-    public final func add(_ hotfolder: consuming Hotfolder) -> Result<Bool, Error> {
+    public final func startWatching() async throws -> Bool {
+        guard !hotfolders.isEmpty else {
+            Self.logger.warning("""
+            \(#file) -> \(#function) \(#line):\(#column)
+            There are no hotfolders to observe.
+            """)
+            return false
+        }
+
+        guard !isRunning else {
+            Self.logger.warning("""
+            \(#file) -> \(#function) \(#line):\(#column)
+            Task is already running.
+            """)
+            return true
+        }
+
+        isRunning = true
+        runLoopTask = Task {
+            do {
+                try await runBackgroundTask()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+
+        Self.logger.info("Background task started.")
+        return isRunning
+    }
+
+    private func runBackgroundTask() async throws {
+        var knownFiles = self.knownFiles
+
+        while !Task.isCancelled {
+            try await Task.sleep(nanoseconds: UInt64(config.watchInterval * 1_000_000_000.0))
+//            Self.logger.log("\(#file) -> \(#function) Looking for changes...")
+
+            for hotfolder in hotfolders {
+                if knownFiles[hotfolder] == nil {
+                    knownFiles[hotfolder] = try await initializeKnownFiles(for: hotfolder)
+                }
+
+                let currentFiles = try getCurrentFiles(for: hotfolder)
+                try await checkForChanges(hotfolder: hotfolder, currentFiles: currentFiles, knownFiles: &knownFiles[hotfolder]!)
+                try await checkForDeletions(hotfolder: hotfolder, currentFiles: currentFiles, knownFiles: &knownFiles[hotfolder]!)
+            }
+        }
+    }
+
+    @discardableResult
+    public final func add(_ hotfolder: Hotfolder) -> Result<Bool, Error> {
         guard hotfolders.count < config.maxHotfolderCount else {
             return .failure(HotfolderWatcherError.maxHotfolderCountReached("Max hotfolder count reached"))
         }
@@ -57,32 +121,23 @@ public final actor HotfolderWatcher: GlobalActor {
         return .success(inserted)
     }
 
-    /// removes a hotfolder by its URL
-    public final func remove(withURl hotfolderUrl: consuming URL) {
-        hotfolders.filter { $0.path == hotfolderUrl }
+    @discardableResult
+    public final func remove(_ hotfolder: Hotfolder) -> Bool {
+        guard let droppedHotfolder = hotfolders.remove(hotfolder) else {
+            Self.logger.warning("""
+            \(#file) -> \(#function) \(#line):\(#column)
+            The given Hotfolder is not contained.
+            """)
+            return false
+        }
+
+        knownFiles.removeValue(forKey: droppedHotfolder)
+        return true
     }
 
     private final func checkForReleasableFiles(in knownFiles: inout [Hotfolder: [String: Date]]) async {
         for hotfolder in hotfolders where !knownFiles.keys.contains(hotfolder) {
             knownFiles.removeValue(forKey: hotfolder)
-        }
-    }
-
-    public final func startWatching() async throws {
-        var knownFiles: [Hotfolder: [String: Date]] = [:]
-
-        while true {
-            try await Task.sleep(nanoseconds: UInt64(config.watchInterval * 1_000_000_000.0))
-            await checkForReleasableFiles(in: &knownFiles)
-
-            for hotfolder in hotfolders {
-                if knownFiles[hotfolder] == nil {
-                    knownFiles[hotfolder] = try await initializeKnownFiles(for: hotfolder)
-                }
-                let currentFiles = try getCurrentFiles(for: hotfolder)
-                try await checkForChanges(hotfolder: hotfolder, currentFiles: currentFiles, knownFiles: &knownFiles[hotfolder]!)
-                try await checkForDeletions(hotfolder: hotfolder, currentFiles: currentFiles, knownFiles: &knownFiles[hotfolder]!)
-            }
         }
     }
 
