724334aada3c2c9c29efcd89d8001b89161dc16a
Christoph <christoph_rohde@icloud.com>
2024-09-21 13:36:33 +0200

    Add Decodable conformance

--- Sources/Swift-Hotfolder/extension/DirectoryEnumerationOptions.swift
+++ Sources/Swift-Hotfolder/extension/DirectoryEnumerationOptions.swift
@@ -7,9 +7,17 @@
 
 import Foundation
 
-extension FileManager.DirectoryEnumerationOptions: Encodable {
+extension FileManager.DirectoryEnumerationOptions: Codable {
+    // Encode DirectoryEnumerationOptions as its raw value (UInt)
     public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
         try container.encode(self.rawValue)
+    }
+
+    // Decode DirectoryEnumerationOptions from its raw value (UInt)
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let rawValue = try container.decode(UInt.self)
+        self.init(rawValue: rawValue)
     }
 }
